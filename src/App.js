b0f04e84f5658a968bea78fb9bd9788a62303c95
Edgar Zapeka <edgar.zapeka@gmail.com>
2018-04-18 14:08:08 -0700

    Added Material UI and React Router Dom libs. Navbar, login & registration components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { withStyles } from 'material-ui/styles'
+import AppBar from 'material-ui/AppBar'
+import Toolbar from 'material-ui/Toolbar'
+import Typography from 'material-ui/Typography'
+import Button from 'material-ui/Button'
+import IconButton from 'material-ui/IconButton'
+import MenuIcon from 'material-ui/Menu'
 
 class App extends Component {
+
   render() {
+
+    const { classes } = this.props
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <div className={classes.root}>
+            <AppBar position="static">
+              <Toolbar>
+                <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="title" color="inherit" className={classes.flex}>
+                  Title
+                </Typography>
+                <Button color="inherit">Login</Button>
+                <Button color="inherit">Registration</Button>
+              </Toolbar>
+            </AppBar>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <h1>The body</h1>
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+export default withStyles(styles)(App);
